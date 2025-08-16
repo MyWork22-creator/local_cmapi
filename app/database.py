97730a2159b44fe44b88da1c4a2a048fb1b91fd9
MyWork22-creator <ngankang2@gmail.com>
2025-08-16 16:32:14 +0700

    da

--- app/database.py
+++ app/database.py
@@ -7,11 +7,11 @@ import urllib.parse
 # Load environment variables from .env
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
+DB_USER = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DB_NAME = os.getenv("DB_DATABASE")
 DB_TYPE = os.getenv("DB_TYPE", "mysql")  # Default to mysql, can be 'sqlite'
 
 
